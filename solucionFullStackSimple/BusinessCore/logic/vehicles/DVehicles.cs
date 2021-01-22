d67fba2864945fa76f8a2b8030287c6ddaaed18c
felipe schneider <felipes88@gmail.com>
2021-01-21 23:19:37 -0300

    Add the Value Object to Vheicle

--- solucionFullStackSimple/BusinessCore/logic/vehicles/DVehicles.cs
+++ solucionFullStackSimple/BusinessCore/logic/vehicles/DVehicles.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessCore.value_objects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,17 +14,21 @@ namespace BusinessCore.logic.vehicles
             vheicles = new Dictionary<Guid, Vehicle>();
         }
 
-        public bool Member(iConn conn, )
+        //TODO: Need to add the Connegtion manager
+        //TODO: See if going with the Abstrac Factory or for now go directly to a SQL Connection and after make the refactoring
+        public bool Member(iConn conn, VOVheicle auxVheicle)
         {
             bool existe = false;
-            if (Find(conn, VOIntegrante) != null)
+            if (Find(conn, auxVheicle) != null)
                 existe = true;
             return existe;
         }
 
-        public Vehicle Find(iConn conn, )
+        public Vehicle Find(iConn conn, VOVheicle auxVheicle )
         {
             Vehicle reponse = null;
+
+            return response;
         }
     }
 }
