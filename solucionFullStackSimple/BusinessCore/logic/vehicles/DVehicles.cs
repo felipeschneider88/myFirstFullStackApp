6b0af68c0d72688741ed230c0c50c72addf84f05
felipe schneider <felipes88@gmail.com>
2021-01-21 23:12:57 -0300

    Add first base for the Dicctionary

---
+++ solucionFullStackSimple/BusinessCore/logic/vehicles/DVehicles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessCore.logic.vehicles
+{
+    class DVehicles
+    {
+        private Dictionary<Guid, Vehicle> vheicles;
+
+        public DVehicles()
+        {
+            vheicles = new Dictionary<Guid, Vehicle>();
+        }
+
+
+    }
+}
