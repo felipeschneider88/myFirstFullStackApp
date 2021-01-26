4b19271b1e99505c3d0532d5d175abae4e5445b9
felipe schneider <felipes88@gmail.com>
2021-01-26 12:11:16 -0300

    correct gitignore and change the connectionManager

---
+++ solucionFullStackSimple/BusinessCore/persistence/ConnectionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BusinessCore.persistence
+{
+    class ConnectionManager
+    {
+        public SqlConnection Conexion
+        {
+            get
+            {
+                string connectionString =
+                    ConfigurationManager.ConnectionStrings["ConnectionStringTalentoDB"].ConnectionString;
+                //string connectionString = "Data Source=localhost;Initial Catalog=TalentosDB;Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+                return conn;
+            }
+        }
+    }
+}
