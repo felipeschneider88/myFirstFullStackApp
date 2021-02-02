846239a37c6b4d3df7c0bdef9cb015c594c0b873
Felipe Schneider <felipes88@gmail.com>
2021-02-02 18:08:21 -0300

    Start to convert from PostgresQL to IConnection

--- solucionFullStackSimple/BusinessCore/logic/vehicles/DVehicles.cs
+++ solucionFullStackSimple/BusinessCore/logic/vehicles/DVehicles.cs
@@ -17,6 +17,38 @@ namespace BusinessCore.logic.vehicles
 
         //TODO: Need to add the Connegtion manager
         //TODO: See if going with the Abstrac Factory or for now go directly to a SQL Connection and after make the refactoring
+        public void Insert(IConnection conn, VOVheicle VOUauxVehicle)
+        {
+            Queries queries = new Queries();
+            string insertUser = queries.InsertUser();
+            NpgsqlCommand insertCommand = new NpgsqlCommand(insertUser, conn);
+            insertCommand.Parameters.Add(
+                new NpgsqlParameter()
+                {
+                    ParameterName = "@email",
+                    DbType = System.Data.DbType.String,
+                    Value = VOUauxVehicle.Email
+                }
+                );
+            insertCommand.Parameters.Add(
+                new NpgsqlParameter()
+                {
+                    ParameterName = "@password",
+                    DbType = System.Data.DbType.String,
+                    Value = VOUauxVehicle.Password
+                }
+                );
+            insertCommand.Parameters.Add(
+                new NpgsqlParameter()
+                {
+                    //See how to cast enum in a proper way
+                    ParameterName = "@type",
+                    DbType = System.Data.DbType.String,
+                    Value = LogicaHelpers.ParseEnumUSerTypeToString(VOUauxVehicle.Type)
+                }
+                ); ;
+            insertCommand.ExecuteNonQuery();
+        }
         public bool Member(IConnection conn, VOVheicle auxVheicle)
         {
             bool existe = false;
