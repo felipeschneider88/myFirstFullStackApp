ab3b2716601e7b81fecad64691bb94f264023891
Felipe Schneider <felipes88@gmail.com>
2021-02-05 19:26:51 -0300

    add appconfig.json para el connectionString al SQL

--- solucionFullStackSimple/BusinessCore/Program.cs
+++ solucionFullStackSimple/BusinessCore/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 namespace BusinessCore
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+
+            var conectionString = config.GetSection("ConnectionString");
+            //string conectionString = "Data Source=localhost;Integrated Security=True";
+                //Configuration.GetConnectionString("DefaultConnection");
+                
+            SqlConnection conn = new SqlConnection(conectionString.Value);
+            conn.Open();
+            // Simple 
+            string sql = "select @@version";
+            using var cmd = new SqlCommand(sql, conn);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"SQL  version: {version}");
         }
+
+        //public static ()
+        //    {
+        //       Console.WriteLine("Testing the connection to the DB:");
+        //    string conectionString =
+        //        Configuration.GetConnectionString("DefaultConnection");
+        //}
     }
 }
